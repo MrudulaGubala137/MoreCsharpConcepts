de9976be86426c90b30c51de04bf58d1fe959e20
PTRL480\Vyshnavi.Gubala <vyshnavi.m@yesgnome.com>
2022-05-10 23:21:05 +0530

    Printing strings and values.

---
+++ Assets/Assignment_C_Sharp/Assignment_C_Sharp/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_C_Sharp
+{
+    internal class Class1
+    {
+       
+        public class Circle
+        {
+            int radius; // Variables
+            float area;
+
+            //Constructor
+            public Circle(int radius)
+            {
+                
+                this.radius = radius;
+                area = (float)(Math.PI * Math.Pow(radius, 2));
+                Console.WriteLine(area);
+               
+
+            }
+            // Getting Parameters
+            public int Radius
+            {
+                get
+                {
+
+                    return radius;
+                }
+            }
+
+            // Getting the area of the circle
+
+            public float Area
+            {
+                get { return area; }
+            }
+        }
+    }
+}
+
---
+++ Assets/Assignment_C_Sharp/Assignment_C_Sharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int radius;
+            float area;
+            radius = 3;
+            area = (float)Math.PI * radius * radius;
+            Console.WriteLine("Radius: " + radius +
+            ", Area: " + area);
+            Console.WriteLine("This is Assignmentn on C-Sharp");
+            Console.WriteLine();
+            Class1.Circle circle=new Class1.Circle(5);
+            Console.ReadLine();
+        }
+    }
+}
